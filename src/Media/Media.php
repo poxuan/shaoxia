e31643eb1a1af7ce77af5a8914e9208de2e3be26
unknown <chentengfei@zhuomogroup.com>
2019-07-31 17:32:59 +0800

    删除冗余

--- src/Media/Media.php
+++ src/Media/Media.php
@@ -8,16 +8,15 @@ class Media
 {
     public function image()
     {
-        $argv = func_get_args();
+        $argv  = func_get_args();
         $angle = $argv[0] ?? 0;     // 偏转角度
-        $color = $argv[1] ?? '000'; // 字体颜色
         $start = microtime(true);
         $image = new Image();
         $pound = Image::px2pound(28); // px 长度转 pound 长度，用于字体
-        $longTest = "陈腾飞 坚持阅读【外刊阅读】12天\n正在和742526人一起学习";
+        $longTest  = "陈腾飞 坚持阅读【外刊阅读】12天\n正在和742526人一起学习";
         $longTest2 = "Chinese State Councilor and Foreign Minister Wang Yi";
         $longTest3 = "与需求结果不一致，但基本完成需求所需模块和扩展要求。倾斜文字加下划线错误";
-        $special  = [
+        $special   = [
             '12' => [
                 'type' => 'color',
                 'color' => '#f00',
@@ -48,16 +47,9 @@ class Media
             ->addText("生成一张分享图片。", 32, 80 , 1466, "#fff", $angle ,'pingfang-standard')
             ->addLayer(80,1500,410,1546,23,Image::COLOR_WHITE)
             ->addText("傻逼需求。。。", 26, 120 , 1535, Image::COLOR_RED, $angle ,'pingfang-slim')
-            ->addPic('./static/img/wechat.jpg',460,1340,220,220) // 在 左上角 添加 750*500 的图片
-            ; // 添加一个圆角浮层,带阴影;
-            // ->addLayerShadow(50,1100,700,1400,30,[1,1]) // 添加一个圆角浮层,带阴影
-            // ->addGradientLayer(50,1100,700,1400,30,"#f0f","#0f0",Image::DIRE_RIGHT | Image::DIRE_LEFT) // 添加渐变浮层
-            // ->addText("哈哈",$pound,350,510, $color, $angle ,'pingfang-standard') // 添加两个字
-            // ->addLongText($longTest, $pound, 75, 555, 600, 45, "", $color, $angle, $special,'pingfang-slim') // 添加长段文字
-            // ->addCriclePic('./static/img/pic1.jpg',375,375,100); // 添加圆图
+            ->addPic('./static/img/wechat.jpg',460,1340,220,220);
         $end = microtime(true);
         echo $image->saveImageLocal('test-'.time().'.jpg','jpeg'); //存入本地
-        $end = microtime(true);
         echo "\ntime cost:" . intval(($end - $start) * 1000) ." ms";
         echo "\nmem  cost:" . round(memory_get_usage() / 1024,2)." KB\n";
     }
