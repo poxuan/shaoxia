99b18f9de68c341f4bc90cd3f1b2a031fb92f5ff
unknown <chentengfei@zhuomogroup.com>
2019-07-31 15:33:51 +0800

    记录错误

--- src/Media/Image/Image.php
+++ src/Media/Image/Image.php
@@ -537,33 +537,75 @@ class Image
         }
     }
 
+    /**
+     * 特殊字符加颜色
+     *
+     * @param [type] $word
+     * @param [type] $params
+     * @param [type] $cur_x
+     * @param [type] $cur_y
+     * @param [type] $size
+     * @param [type] $angle
+     * @param [type] $color
+     * @param [type] $fontfile
+     * @param [type] $box
+     * @return void
+     * @author chentengfei
+     * @since
+     */
     public function specialColor($word, $params, $cur_x, $cur_y, $size, $angle, $color, $fontfile, $box) {
         $color2 = $this->getColor($params['color']);
         imagefttext($this->image, $size, $angle, $cur_x, $cur_y, $color2, $fontfile, $word);
     }
 
+    /**
+     * 特殊字符换字体
+     *
+     * @param [type] $word
+     * @param [type] $params
+     * @param [type] $cur_x
+     * @param [type] $cur_y
+     * @param [type] $size
+     * @param [type] $angle
+     * @param [type] $color
+     * @param [type] $fontfile
+     * @param [type] $box
+     * @return void
+     * @author chentengfei
+     * @since
+     */
     public function specialFont($word, $params, $cur_x, $cur_y, $size, $angle, $color, $fontfile, $box) {
         $fontfile2 = $this->getFont($params['font']);
         imagefttext($this->image, $size, $angle, $cur_x, $cur_y, $color, $fontfile2, $word);
     }
 
+    /**
+     * 特殊字符加下划线
+     *
+     * @param [type] $word
+     * @param [type] $params
+     * @param [type] $cur_x
+     * @param [type] $cur_y
+     * @param [type] $size
+     * @param [type] $angle
+     * @param [type] $color
+     * @param [type] $fontfile
+     * @param [type] $box
+     * @return void
+     * @author chentengfei
+     * @since
+     */
     public function specialUnderline($word, $params, $cur_x, $cur_y, $size, $angle, $color, $fontfile, $box) {
         $box_width = sqrt(pow($box[2] + 1, 2) + pow($box[3] + 1, 2));
         $width  = $params['width'] ?? 1;
         $sx = $cur_x + $width * sin(deg2rad($angle));
         $sy = $cur_y - $width * cos(deg2rad($angle));
-        $ex = $cur_x + $box_width * cos(deg2rad($angle));
+        $ex = $cur_x + $box_width * cos(deg2rad($angle)) + $width * sin(deg2rad($angle));
         $ey = $cur_y - $box_width * sin(deg2rad($angle)) + $width * cos(deg2rad($angle));
+        // var_dump($sx,$sy,$ex,$ey);
         $this->addLayer($sx , $sy ,$ex , $ey, $width / 2 , $params['color'] ?? '#fff');
         imagefttext($this->image, $size, $angle, $cur_x, $cur_y, $color, $fontfile, $word);
     }
-
-    // public function specialFont($word, $params, $cur_x, $cur_y, $size, $angle, $color, $fontfile) {
-    //     $fontfile2 = $this->getFont($params['font']);
-    //     imagefttext($this->image, $size, $angle, $cur_x, $cur_y, $color, $fontfile2, $word);
-    // }
-
-
 
     /**
      * 按比例压缩(文件体)
