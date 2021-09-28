f4c1a7a3a113ec67ca780ade5239731867c4ecbb
chentengfei <958718201@qq.com>
2021-09-28 13:35:48 +0800

    bugfix

--- config/app.php
+++ config/app.php
@@ -9,8 +9,8 @@ return [
     ],
     // 中间件
     "middleware" => [
-        Shaoxia\Middleware\Test1::class,
-        Shaoxia\Middleware\Test2::class,
+        // Shaoxia\Middleware\Test1::class,
+        // Shaoxia\Middleware\Test2::class,
     ],
     // 路由中间件
     'route_middleware' => [
--- src/Middleware/Test2.php
+++ src/Middleware/Test2.php
@@ -5,9 +5,9 @@ namespace Shaoxia\Middleware;
 class Test2 
 {
     function handle($request, $next) {
-        echo "test 2.1\n";
+        // echo "test 2.1\n";
         $result = $next ? $next($request) : null;
-        echo "test 2.2\n";
+        // echo "test 2.2\n";
         return $result;
     }
 }
