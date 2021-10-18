3c33327bbfd86106af1dba6f4d299e9a16591978
chentengfei <958718201@qq.com>
2021-10-18 17:18:16 +0800

    优化

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 // 定义路径
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT_PATH", __DIR__.DS);
+define("APP_PATH", ROOT_PATH.'app'.DS);
 define("CONFIG_PATH", ROOT_PATH.'config'.DS);
 define("ROUTER_PATH", ROOT_PATH.'router'.DS);
-define("SRC_PATH", ROOT_PATH.'src'.DS);
 define("STORAGE_PATH", ROOT_PATH.'storage'.DS);
-define("TMP_PATH", ROOT_PATH.'tmp'.DS);
+define("TMP_PATH", STORAGE_PATH.'tmp'.DS);
 
 
 require ROOT_PATH . '/vendor/autoload.php';
-require ROOT_PATH . '/helper.php';
 
 app()->exec();
--- public/index.php
+++ public/index.php
@@ -2,15 +2,12 @@
 
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT_PATH", dirname(__DIR__).DS);
+define("APP_PATH", ROOT_PATH.'app'.DS);
 define("CONFIG_PATH", ROOT_PATH.'config'.DS);
 define("ROUTER_PATH", ROOT_PATH.'router'.DS);
-define("SRC_PATH", ROOT_PATH.'src'.DS);
 define("STORAGE_PATH", ROOT_PATH.'storage'.DS);
-define("TMP_PATH", ROOT_PATH.'tmp'.DS);
+define("TMP_PATH", STORAGE_PATH.'tmp'.DS);
 
 require ROOT_PATH . '/vendor/autoload.php';
-require ROOT_PATH . '/helper.php';
 
-$app = require ROOT_PATH.'/bootstrap.php';
-
-$app->exec();
+app()->exec();
